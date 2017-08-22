5e61f1e104fd19db12f180cd685af6e9971fbe4e
Diona Kurien <diona.david@gmail.com>
2017-08-21 21:56:39 -0400

    hangman logic completed

--- script.js
+++ script.js
@@ -1,68 +1,77 @@
-var correctWord = ['cattA', 'vasddfB', 'C'];
+var words = ['pizza', 'onion', 'tomato', 'olives', 'chicken'];
 var userWord = [];
 var score = 0;
 var chances = 7;
 var count = 0;
-var j;
-var hangMan = ['head', 'body', 'arm1', 'arm2', 'leg1', 'leg2'];
+var splitWords = [];
+var currentWord = [];
+// generate random word
+function generateWord() {
+    currentWord = words[Math.floor(Math.random() * words.length)];
+    //generate blank spaces
+    splitWords = currentWord.split('');
+    console.log(splitWords);
+    console.log(currentWord);
+    chances = splitWords.length;
+    for (var j = 0; j < splitWords.length; j++) {
+        userWord[j] = "_ ";
+    }
+    document.getElementById("resultLbl").innerText = userWord;
 
+}
+$("#guess").on("click", function(e) {
+    e.preventDefault();
+    guessLetter(document.getElementById("letter").value);
+});
 
+//user inputs and checking for match
 function guessLetter(letter) {
-    letter = letter.toUpperCase();
-    for (var i = 0; i < correctWord.length; i++) {
+    letter = letter.toLowerCase();
+    for (var i = 0; i < splitWords.length; i++) {
         // check if the user word matches to any word in the correctword array.
-        if (correctWord.includes(letter)) {
-
-            userWord.push(letter);
-            correctWord.splice(i, 1); // so that user cant enter a value twice
-            console.log(correctWord);
+        if (splitWords.includes(letter)) {
+            //splitWords.splice(i, 1); // so that user cant enter a value twice
             console.log("match");
-            console.log(userWord);
-            count = count + 1;
-            console.log(score);
-            checkMatch();
+            checkMatchLetter(letter);
             return true;
-
 
         } else {
             count = count + 1;
             console.log("wrong match");
-
             score = chances - count;
             //console.log(`you lost ${hangMan[j]}`)
             console.log(`you have ${score} tries`)
             endChance();
-
-
-            console.log(count);
             return false;
         }
 
     }
 }
 
-function checkMatch() {
-    console.log("check function")
-    if (correctWord.length === 0) {
-        console.log("you got it right");
-        console.log(`tries you took ${count}`);
+function checkMatchLetter(letterCh) {
+    for (var i = 0; i < splitWords.length; i++) {
+        if (splitWords[i] == letterCh)
+            userWord[i] = letterCh;
     }
+    document.getElementById("resultLbl").innerText = userWord;
+    checkMatch();
+}
 
+
+function checkMatch() {
+    for (var i = 0; i < userWord.length; i++) {
+        if (userWord[i] === "_ ")
+            return;
+    }
+    console.log("you got it right");
+    console.log(`tries you took ${count}`);
 }
 
 function endChance() {
     if (score === 0) {
         console.log("you have reached your max tries")
+        console.log(`The word is ${currentWord}`)
     }
 }
 
-function blankSpaces(){
-
-}
-guessLetter('a');
-guessLetter('d');
-guessLetter('e');
-guessLetter('b');
-guessLetter('k');
-guessLetter('h');
-guessLetter('l');
+generateWord();
