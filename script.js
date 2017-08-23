9a1c9d52de6bd78a33daacfa5584195a14c156b2
Diona Kurien <diona.david@gmail.com>
2017-08-22 20:14:15 -0400

    hangman css file complete

--- script.js
+++ script.js
@@ -1,51 +1,106 @@
-var words = ['pizza', 'onion', 'tomato', 'olives', 'chicken'];
-var userWord = [];
-var score = 0;
-var chances = 7;
+/*var words = { movies:   ['jaws','titanic','back to future'],
+              food:     ['pizza','burger','hot dog','noodels'],
+              sports:   ['basket ball','hockey','soccer','tennis']
+            }
+*/
+
+
+
+var words = ['pizza', 'onion', 'tomato', 'olives', 'chicken','hot dog'];
+var userWord = []; //empty array to generate blank spaces
+var score = 0; //t track your chances
+var chances = 6; // max cahnce to try
 var count = 0;
-var splitWords = [];
-var currentWord = [];
+var splitWords = []; // splits each letter in a word
+var currentWord = []; // stores the random generated word
+var guessAlpabet = [];
 // generate random word
 function generateWord() {
     currentWord = words[Math.floor(Math.random() * words.length)];
-    //generate blank spaces
     splitWords = currentWord.split('');
     console.log(splitWords);
-    console.log(currentWord);
-    chances = splitWords.length;
+    //console.log(currentWord);
+    var hangmanWord='';
     for (var j = 0; j < splitWords.length; j++) {
-        userWord[j] = "_ ";
+        userWord[j] = splitWords[j]==' '?' ':"_ ";
+        hangmanWord=hangmanWord+"_ ";
     }
-    document.getElementById("resultLbl").innerText = userWord;
-
+    document.getElementById("resultLbl").innerText = hangmanWord;
 }
+//event listener
 $("#guess").on("click", function(e) {
     e.preventDefault();
-    guessLetter(document.getElementById("letter").value);
+    if(document.getElementById("letter").value.trim() == '')
+    return;
+    guessLetter(document.getElementById("letter").value.trim());
+
+    $("#letter").val('')
 });
+$("#restart").click(restart);
+
+function restart() {
+    score = 0;
+    count = 0;
+
+    $("#hangImg").attr("src", "images/hang_1.gif");
+    $("#letter").val('');
+    guessAlpabet=[];
+    userWord = [];
+    document.getElementById("guessLet").innerText = guessAlpabet;
+    generateWord();
+}
+
 
 //user inputs and checking for match
 function guessLetter(letter) {
+  guessAlpabet.push(letter);
+  document.getElementById("guessLet").innerText = guessAlpabet;
+  console.log(guessAlpabet);
     letter = letter.toLowerCase();
     for (var i = 0; i < splitWords.length; i++) {
         // check if the user word matches to any word in the correctword array.
         if (splitWords.includes(letter)) {
-            //splitWords.splice(i, 1); // so that user cant enter a value twice
-            console.log("match");
             checkMatchLetter(letter);
             return true;
 
         } else {
+
             count = count + 1;
-            console.log("wrong match");
             score = chances - count;
-            //console.log(`you lost ${hangMan[j]}`)
-            console.log(`you have ${score} tries`)
+            alert(`you have ${score} more tries`);
             endChance();
-            return false;
+            hangMan();
+            return;
         }
 
     }
+}
+
+function hangMan(){
+  switch (count) {
+
+      case 1:
+          $("#hangImg").attr("src", "images/hang_2n.gif");
+          break;
+      case 2:
+          $("#hangImg").attr("src", "images/hang_3n.gif");
+          break;
+      case 3:
+          $("#hangImg").attr("src", "images/hang_4n.gif");
+          break;
+      case 4:
+          $("#hangImg").attr("src", "images/hang_5n.gif");
+          break;
+      case 5:
+          $("#hangImg").attr("src", "images/hang_6n.gif");
+          break;
+      case 6:
+          $("#hangImg").attr("src", "images/hang_7n.gif");
+          break;
+      default:
+          alert("no match");
+          break;
+  }
 }
 
 function checkMatchLetter(letterCh) {
@@ -53,7 +108,11 @@ function checkMatchLetter(letterCh) {
         if (splitWords[i] == letterCh)
             userWord[i] = letterCh;
     }
-    document.getElementById("resultLbl").innerText = userWord;
+    var hangmanWord='';
+    for (var j = 0; j < userWord.length; j++) {
+        hangmanWord=hangmanWord+userWord[j];
+    }
+    document.getElementById("resultLbl").innerText = hangmanWord;
     checkMatch();
 }
 
@@ -63,15 +122,20 @@ function checkMatch() {
         if (userWord[i] === "_ ")
             return;
     }
-    console.log("you got it right");
-    console.log(`tries you took ${count}`);
+    alert("you got it right");
+    alert(`tries you took ${count}`);
+
 }
 
 function endChance() {
     if (score === 0) {
-        console.log("you have reached your max tries")
-        console.log(`The word is ${currentWord}`)
+        alert("you have reached your max tries")
+        alert(`The word is ${currentWord}`)
+
     }
+}
+function strikes(){
+
 }
 
 generateWord();
