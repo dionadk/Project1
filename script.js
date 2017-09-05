82696f87c60c0bf1689e0bd8b0d46678906e2b7f
aspittel <ali07cat07@gmail.com>
2017-09-05 08:54:58 -0400

    feedback

--- script.js
+++ script.js
@@ -9,6 +9,22 @@ myWords.movies = ['jaws', 'titanic', 'back to the future', 'forrest gump', 'star
 myWords.food = ['pizza', 'burger', 'hot dog', 'noodles', 'pasta', 'salad', 'ravioli', 'buritto','soup'];
 myWords.sports = ['basket ball', 'hockey', 'soccer', 'tennis', 'base ball', 'rugby', 'cricket', 'volley ball'];
 
+/*
+AS: 
+The way you are using this object, it would probably be best to use
+an object literal, so
+
+let myWords = {
+  movies: ['jaws', 'titanic', 'back to the future', 
+    'forrest gump', 'star wars', 'rocky', 'the dark knight'],
+  food: ['pizza', 'burger', 'hot dog', 'noodles', 'pasta', 
+    'salad', 'ravioli', 'buritto', 'soup'],
+  sports: ['basket ball', 'hockey', 'soccer', 
+    'tennis', 'base ball', 'rugby', 'cricket', 'volley ball']
+}
+*/
+
+// AS - I would be consistent here and have comments for each of these
 var userWord = []; //empty array to generate blank spaces
 var score = 0; //t track your chances
 var chances = 6; // max cahnce to try
@@ -20,25 +36,32 @@ var words = myWords.food;
 var win = 0;
 var loss = 0;
 
-//event listener----
+//event listener
 //selct dropdown list
 $("#selCategory").on("change", function(e) {
+  // AS - why is this variable called res? I would use the full word instead
+  // of an abbreviation
   var res = e.currentTarget.value;
+  /* AS - These should be indented, you could also 
+    do return myWords[res] which would reduce these 
+    10 lines to 1 */
   switch (res) {
     case 'movies':
-    words = myWords.movies;
-    break;
+      words = myWords.movies;
+      break;
     case 'sports':
-    words = myWords.sports;
-    break;
+      words = myWords.sports;
+      break;
     default:
-    words = myWords.food;
-    break;
+      words = myWords.food;
+      break;
   }
   restart();
   //generateWord();
 
 });
+
+
 // event listener to call the guess letter function
 $("#guess").on("click", function(e) {
   e.preventDefault();
@@ -50,6 +73,7 @@ $("#guess").on("click", function(e) {
 });
 $("#restart").click(restart);
 
+
 function restart() {
   score = 0;
   count = 0;
@@ -58,13 +82,18 @@ function restart() {
   $("#letter").val('');
   guessAlphabet = [];
   userWord = [];
+  // AS - be consistent in using either JQuery or Vanilla JS's Dom manipulation.
   document.getElementById("guessLet").innerText = guessAlphabet;
   generateWord();
 }
+
+/* AS - Be consistent in spacing between functions */
+
 // generate random word
 function generateWord() {
   var selectWord = '';
   do {
+    // AS - Good use of the do while loop!
     selectWord = words[Math.floor(Math.random() * words.length)];
   } while (currentWord === selectWord);
   currentWord = selectWord;
@@ -79,6 +108,7 @@ function generateWord() {
 }
 
 //user inputs and checking for match
+// AS - I would break this up -- really long function!
 function guessLetter(letter) {
   if (chances - count === 0)
   return;
@@ -101,7 +131,8 @@ function guessLetter(letter) {
       count = count + 1;
       score = chances - count;
       $(".error-messages").html(`YOU HAVE ${score} TRIES`).fadeIn(2000);
-      $(".error-messages").html(`YOU HAVE ${score} TRIES`).fadeOut(1000);
+      // AS - no need to reset the HTML content here.
+      $(".error-messages").fadeOut(1000);
       //alert(`you have ${score} tries`);
       endChance();
       hangMan();
@@ -130,13 +161,15 @@ function checkMatch() {
     return;
   }
   $(".error-messages").html(`CONGRATS!!!YOU WIN`).fadeIn(3000);
-  $(".error-messages").html(`CONGRATS!!!YOU WIN`).fadeOut(4000);
+  $(".error-messages").fadeOut(4000);
   win++;
   $("#winSummary").text(win);
 }
 
 //assigning parts to the man
 function hangMan() {
+  // AS - $("#hangImg").attr("src", `images/hang_`${count+1}n.gif"`)
+  // would cover all of this.
   switch (count) {
 
     case 1:
@@ -165,14 +198,23 @@ function hangMan() {
 // game over
 function endChance() {
   if (score === 0) {
+    /*
+    AS - would shorten this line here
+
+    let errorMessage = $(".error-messages")
+    errorMessage.html(`HANGED!!!YOU HAVE REACHED YOUR MAX TRIES.
+      THE WORD IS ${currentWord.toUpperCase()}`)
+    errorMessage.fadeIn(3000).fadeOut(5000)
+    */
     $(".error-messages").html(`HANGED!!!YOU HAVE REACHED YOUR MAX TRIES.THE WORD IS ${currentWord.toUpperCase()}`).fadeIn(3000);
-    $(".error-messages").html(`HANGED!!!YOU HAVE REACHED YOUR MAX TRIES.THE WORD IS ${currentWord.toUpperCase()}`).fadeOut(5000);
+    $(".error-messages").fadeOut(5000);
     loss++;
     $("#lossSummary").text(loss);
     console.log(loss);
   }
 }
 
+// AS - remove dead code!
 function setScore() {
 
 }
